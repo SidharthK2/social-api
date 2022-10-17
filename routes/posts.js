afca1533482b4bacf4f5e1dcf24ecb5c424d6eea
sidharthK2 <sidharthkapoor.100@gmail.com>
2022-10-18 00:12:34 +0530

    add user query

--- routes/posts.js
+++ routes/posts.js
@@ -90,7 +90,7 @@ router.get("/timeline/:userId", async (req, res) => {
 router.get("/profile/:username", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username });
-    const posts = Post.find({ userId: user._id });
+    const posts = await Post.find({ userId: user._id });
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json(error);
